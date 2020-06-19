ccdb05b4c8b500e916a3138d257d7490d1ff5d2b
Anderson <upmalagutti@gmail.com>
2020-06-19 13:09:28 +0200

    Add Cell and Label, and connect them to the CollectionViewCell class

---
+++ CollectionViews/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Anderson Malaguitte on 19/06/2020.
+//  Copyright © 2020 Anderson. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
